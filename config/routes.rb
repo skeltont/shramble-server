b0abd3d54b6e7e2842a7bdf721a0c13153d95244
Ty Skelton <4512337+skeltont@users.noreply.github.com>
2022-03-27 20:59:51 -0700

    removes references to rswag from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  # mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Api::Engine => '/api-docs'
 
   resources :room, only: [:create, :index]
   post 'join', to: 'room#join'
